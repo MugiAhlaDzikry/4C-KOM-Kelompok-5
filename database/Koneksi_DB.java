a925829646d381cc08ee1c989a594431a77c6d51
Mugiahla <65592011+MugiAhlaDzikry@users.noreply.github.com>
2025-06-07 17:11:16 +0700

    Update Koneksi_DB.java

--- database/Koneksi_DB.java
+++ database/Koneksi_DB.java
@@ -1,4 +1,20 @@
 package database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Koneksi_DB {
+    private static final String URL = "jdbc:mysql://localhost:3306/db_ayamgoreng";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException ex) {
+            System.out.println("Connection Error: " + ex.getMessage());
+            return null;
+        }
+    }
 }

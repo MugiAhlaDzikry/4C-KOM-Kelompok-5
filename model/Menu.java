57e45767db7f87361420384ee30dda15e70062c4
Mugiahla <65592011+MugiAhlaDzikry@users.noreply.github.com>
2025-06-07 17:19:55 +0700

    Create Menu.java

---
+++ model/Menu.java
@@ -0,0 +1,33 @@
+package model;
+
+public class Menu {
+    private int id;
+    private String nama;
+    private double harga;
+
+    public Menu(int id, String nama, double harga) {
+        this.id = id;
+        this.nama = nama;
+        this.harga = harga;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+}
